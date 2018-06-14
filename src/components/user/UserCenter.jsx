266ffea874c8566426ba00db3d1e5cf7e1c884a9
wfy0301 <1398871006@qq.com>
2018-06-14 15:12:41 +0800

    清除浮动

--- src/components/user/UserCenter.jsx
+++ src/components/user/UserCenter.jsx
@@ -21,7 +21,7 @@ export default class User extends Component {
   render() {
     let match = this.props.match
     return (
-      <div className="clearfix">
+      <div className="clearfix inner">
         <ul className="user-nav fl">
           <li><Link to={`${match.url}/safe`}>安全中心</Link></li>
           <li><Link to={`${match.url}/identity`}>身份认证</Link></li>
