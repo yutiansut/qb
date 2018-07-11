8332d7f577b9b788617a1d89a1f6c589e556f755
DevilRoshan <1163767451@qq.com>
2018-07-11 15:33:47 +0800

    首页修复

--- src/components/notice/HomeNotice.jsx
+++ src/components/notice/HomeNotice.jsx
@@ -67,17 +67,16 @@ export default class homeNotice extends exchangeViewBase {
   render() {
     // console.log('首页公告', this.state)
     return <div className={`${this.state.noticeList && this.state.noticeList.length ? "" : "hide"} home-notice-wrap`}>
-      {this.state.noticeList && this.state.noticeList.length && <div className="home-notice-content clearfix">
-        <p>{this.intl.get("notice")}</p>
+      {this.state.noticeList && this.state.noticeList.length && <div className="home-notice-content">
         <ul style={{top: this.state.top1 + "%"}}>
-          {this.state.noticeList.map((v, index) => <li key={index}>
+          {this.state.noticeList.map((v, index) => <li key={index}><i>{this.intl.get("notice")}</i>
             <Link to={`notice/content/detail?noticeId=${v.activityId}`}>
               {this.props.controller.configData.language === "zh-CN" ? v.subjectCn : v.subjectEn}
             </Link>
           </li>)}
         </ul>
         <ul style={{top: this.state.top2 + "%"}}>
-          {this.state.noticeList.map((v, index) => <li key={index}>
+          {this.state.noticeList.map((v, index) => <li key={index}><i>{this.intl.get("notice")}</i>
             <Link to={`notice/content/detail?noticeId=${v.activityId}`}>
               {this.props.controller.configData.language === "zh-CN" ? v.subjectCn : v.subjectEn}
             </Link>
