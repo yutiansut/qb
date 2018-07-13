7a20895c4080751c58d3db0324d576d69f4ce196
wfy0301 <1398871006@qq.com>
2018-07-13 17:36:41 +0800

    登录两步认证修改

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -215,7 +215,7 @@ export default class Login extends exchangeViewBase {
             autoClose = {true}
           />
         )}
-        {this.state.showTwoVerify && <TwoVerifyPopup verifyNum={this.state.verifyNum} type={verifyTypeObj[this.state.verifyType]} getVerify={() => {this.getVerify(this.state.twoVerifyUser, this.state.userType, 0)}} onClose={() => {
+        {this.state.showTwoVerify && <TwoVerifyPopup verifyNum={this.state.verifyNum} type={verifyTypeObj[this.state.verifyType]} getVerify={() => {this.getVerify(this.state.twoVerifyUser, this.state.verifyType === 2009 ? 1 : 0,  0)}} onClose={() => {
           this.setState({ showTwoVerify: false });
         }} destroy={this.destroy} onConfirm={code => {
           this.login(this.state.verifyType === 2008 ? this.state.userInput : this.state.twoVerifyUser, code, this.state.verifyType === 2008 ? this.state.userType : (this.state.verifyType === 2009 ? 1 : 0), this.state.verifyType === 2008 ? 2 : 3, this.state.captchaId, this.state.picInput);
