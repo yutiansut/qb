40634ef1c257de97df2bcc7eefef703c55d57cc4
xiongzg <xiongzg@mixotc.com>
2018-07-17 15:47:00 +0800

    get color

--- src/components/headerAndFooter/Footer.jsx
+++ src/components/headerAndFooter/Footer.jsx
@@ -35,26 +35,26 @@ export default class Footer extends ExchangeViewBase {
 
   render() {
     return (
-      <div className="footer-wrap clearfix">
-        <div className="fl">
+      <div className="footer-wrap">
+        <div>
           <Link to='/whome' className="home-img"><img src="/static/img/logo-black.svg" alt="" /></Link>
-          <ul className="clearfix">
+          <ul>
             {this.footerArray.map((item, index) => (<li key={index}>
               <Link to={item.to}>{item.label}</Link>
             </li>))}
           </ul>
-          <ol className="clearfix">
+        </div>
+        <div className="right-content">
+          {/* <p>{this.intl.get('footer-risk')}</p> */}
+          <p>© 2018 QB.com . All rights reserved</p>
+        </div>
+        <ol className="clearfix">
             <li><a href="https://twitter.com/QB_Exchange" target="_blank"><img src="/static/img/footer/twitter.svg" alt="" /></a></li>
             <li><a href="https://www.facebook.com/qbexchange/" target="_blank"><img src="/static/img/footer/FaceBook.svg" alt="" /></a></li>
             {/*<li><img src="/static/img/footer/fill.svg" alt="" /></li>*/}
             <li><img src="/static/img/footer/wx.svg" alt="" className="wx-img" /><i className="wx-qrCode"></i></li>
             <li><a href="https://t.me/QB_ExchangeEN" target="_blank"><img src="/static/img/footer/Telegram.svg" alt="" /></a></li>
           </ol>
-        </div>
-        <div className="fr right-content">
-          <p>{this.intl.get('footer-risk')}</p>
-          <p>© 2018 QB.com . All rights reserved</p>
-        </div>
       </div>
     )
   }
