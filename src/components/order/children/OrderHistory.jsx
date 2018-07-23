aa593f3af967567106617973ee30306ae5f679d5
wfy0301 <1398871006@qq.com>
2018-07-23 20:45:37 +0800

    order文件找回

---
+++ src/components/order/children/OrderHistory.jsx
@@ -0,0 +1,27 @@
+import ExchangeViewBase from '../../ExchangeViewBase'
+import React, { Component } from "react";
+
+export default class OrderHistory extends ExchangeViewBase{
+  constructor(props){
+    super(props);
+    this.state = {
+    
+    };
+    const {controller} = this.props;
+    //绑定view
+    controller.setView(this);
+    //初始化数据，数据来源即store里面的state
+    this.state = Object.assign(this.state, controller.initState);
+  }
+  componentWillMount(){
+  
+  }
+  componentDidMount(){
+  
+  }
+  render() {
+    return(
+        <h3>lishi</h3>
+    )
+  }
+}
