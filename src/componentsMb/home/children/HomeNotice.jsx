dcfd62b98ba9c9100b0adda40042d781a89a187e
jiangcj <398017990@qq.com>
2018-07-25 14:32:15 +0800

    h5-首页消息通知回滚

--- src/componentsMb/home/children/HomeNotice.jsx
+++ src/componentsMb/home/children/HomeNotice.jsx
@@ -3,78 +3,80 @@ import {Link} from 'react-router-dom'
 import ExchangeViewBase from '../../../components/ExchangeViewBase'
 
 export default class HomeNotice extends ExchangeViewBase {
-  constructor(props) {
-    super(props);
-    this.state = {
-      top1: 0,
-      top2: 100,
-      criticalArr: [0, 100]
-    };
-    const {controller} = props;
-    // 绑定view
-    controller.setView(this);
-    // 初始化数据，数据来源即store里面的state
-    this.state = Object.assign(this.state, controller.initState);
-    this.getNoticeCon = controller.getNoticeCon.bind(controller) // 获取公告
-  }
-
-  componentWillMount(){}
-
-  async componentDidMount() {
-    await this.getNoticeCon(0, 5);
-    let result = Object.keys(this.state.noticeList).length && this.state.noticeList.data || []
-    if (Object.keys(this.state.noticeList).length) {
-      this.setState(
-        {
-          top2: Math.ceil(result.length) * 100,
-          criticalArr: Array.from(
-            { length: Math.ceil(result.length + 1) },
-            (item, index) => index * 100
-          )
-        },
-        () => {
-          this.props.controller.swiper(
-            "carousel",
-            this,
-            "top1",
-            "top2",
-            this.state.criticalArr,
-            10,
-            3000
-          );
-        }
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            top1: 0,
+            top2: 100,
+            criticalArr: [0, 100]
+        };
+        const {controller} = props;
+        // 绑定view
+        controller.setView(this);
+        // 初始化数据，数据来源即store里面的state
+        this.state = Object.assign(this.state, controller.initState);
+        this.getNoticeCon = controller.getNoticeCon.bind(controller) // 获取公告
     }
-  }
 
-  componentWillUpdate(...parmas) {
+    componentWillMount(){}
 
-  }
+    async componentDidMount() {
+        await this.getNoticeCon(0, 5);
+        let result = this.state.noticeList;
+        if (this.state.noticeList && this.state.noticeList.length) {
+            this.setState({
+                    top2: Math.ceil(result.length) * 100,
 
-  componentWillUnmount() {
-    this.props.controller.swiperStop("carousel");
-  }
+                    criticalArr: Array.from(
+                        {length: Math.ceil(result.length + 1)},
+                        (item, index) => index * 100
+                    )
+                },
+                () => {
+                    this.props.controller.swiper(
+                        "carousel",
+                        this,
+                        "top1",
+                        "top2",
+                        this.state.criticalArr,
+                        10,
+                        3000
+                    );
+                }
+            );
+        }
+    }
 
-  render() {
-    return <div className={`${Object.keys(this.state.noticeList).length && this.state.noticeList.data ? "" : "hide"} home-notice-wrap`}>
-      {Object.keys(this.state.noticeList).length && this.state.noticeList.data && <div className="home-notice-content">
-        <ul style={{ top: this.state.top1 + "%" }}>
-          {Object.keys(this.state.noticeList).length && this.state.noticeList.data && this.state.noticeList.data.map((v, index) => <li key={index}>
-            <Link to={`wnotice/content/detail?noticeId=${v.activityId}`}>
-              <i>【{v.createdAt.toDate('MM-dd')}】</i>
-              <span>{this.props.controller.configData.language === "zh-CN" ? v.subjectCn : v.subjectEn}</span>
-            </Link>
-          </li>)}
-        </ul>
-        <ul style={{ top: this.state.top2 + "%" }}>
-          {Object.keys(this.state.noticeList).length && this.state.noticeList.data && this.state.noticeList.data.map((v, index) => <li key={index}>
-            <Link to={`wnotice/content/detail?noticeId=${v.activityId}`}>
-              <i>【{v.createdAt.toDate('MM-dd')}】</i>
-              <span>{this.props.controller.configData.language === "zh-CN" ? v.subjectCn : v.subjectEn}</span>
-            </Link>
-          </li>)}
-        </ul>
-      </div>}
-    </div>;
-  }
+    componentWillUpdate(...parmas) {
+
+    }
+
+    componentWillUnmount() {
+        this.props.controller.swiperStop("carousel");
+    }
+
+    render() {
+        let noticeList = this.state.noticeList ? this.state.noticeList.data : [];
+        let lang = this.props.controller.configData.language;
+        return <div className={`${noticeList && noticeList.length ? "" : "hide"} home-notice-wrap`}>
+            {noticeList && <div className="home-notice-content">
+                <ul style={{top: this.state.top1 + "%"}}>
+                    {noticeList.map((v, index) => <li key={index}>
+                        <a>
+                            <img src="/static/mobile/home/img_lb@2x.png"/>
+                            <span>{lang === "zh-CN" ? v.subjectCn : v.subjectEn}</span>
+                        </a>
+                    </li>)}
+                </ul>
+                <ul style={{top: this.state.top2 + "%"}}>
+                    {noticeList.map((v, index) => <li key={index}>
+                        <a>
+                            <img src="/static/mobile/home/img_lb@2x.png"/>
+                            <span>{lang === "zh-CN" ? v.subjectCn : v.subjectEn}</span>
+                        </a>
+                    </li>)}
+                </ul>
+            </div>}
+        </div>;
+    }
 }
