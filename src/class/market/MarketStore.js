c3d16e42cef2684c12718b818bf337f7072c5981
DevilRoshan <1163767451@qq.com>
2018-07-27 14:03:16 +0800

    市场涨跌颜色不消失修复

--- src/class/market/MarketStore.js
+++ src/class/market/MarketStore.js
@@ -176,7 +176,7 @@ export default class MarketStore extends ExchangeStoreBase {
       //除第一次外需要计算增长值改变字体颜色
       if (type) {
         // console.log('计算涨跌', res.price, v.price)
-        res.updown = res.price - v.price
+        res.updown = (res.price - v.price) || res.updown
       }
       return Object.assign(v, res)
     }))
