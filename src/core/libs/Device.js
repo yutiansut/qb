ffb90d502c2e190b0af13d1912fbad472f51ba46
DevilRoshan <1163767451@qq.com>
2018-08-03 08:54:44 +0800

    浏览器兼容

--- src/core/libs/Device.js
+++ src/core/libs/Device.js
@@ -10,4 +10,69 @@ import Device from 'current-device'
 
 export default Device;
 
+function bro() {
+    var strStart = 0, strStop = 0, device = '', version = '';
+
+    var userAgent = window.navigator.userAgent; //包含以下属性中所有或一部分的字符串：appCodeName,appName,appVersion,language,platform
+
+    if (userAgent.indexOf('Firefox') != -1) {
+        strStart = userAgent.indexOf('Firefox');
+        device = 'Firefox';
+    }
+
+    if (userAgent.indexOf('Safari') != -1) {
+        if (userAgent.indexOf("Chrome") != -1) {
+            strStart = userAgent.indexOf('Chrome');
+            strStop = userAgent.indexOf(' Safari');
+            device = 'Chrome';
+        } else {
+            strStart = userAgent.indexOf('Version');
+            strStop = userAgent.indexOf(' Safari');
+            device = 'Safari';
+        }
+    }
+
+    if (userAgent.indexOf('Edge') != -1) {
+        strStart = userAgent.indexOf('Edge');
+        strStop  = userAgent.length
+        device = 'Edge';
+    }
+
+    strStop  = strStop || userAgent.length
+    version = userAgent.substring(strStart, strStop).split('/')[1].split('.')[0];
+    return {version, device}
+}
+
+function dynamicLoadJs(url, callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    if (typeof(callback) == 'function') {
+        script.onload = script.onreadystatechange = function () {
+            if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") {
+                callback();
+                script.onload = script.onreadystatechange = null;
+            }
+        };
+    }
+    head.appendChild(script);
+}
+
+// var broScript = bro()
+//
+var broDicScript = {
+    Chrome: 51,
+    Safari: 10,
+    Edge: 14,
+    Firefox: 53,
+}
+// if(!broScript.device || broScript.version < broDicScript[broScript.device]){
+//     dynamicLoadJs("/static/profill/es5-shim.min.js");
+//     dynamicLoadJs("/static/profill/es5-sham.min.js");
+//     dynamicLoadJs("/static/profill/es6-shim.min.js");
+//     dynamicLoadJs("/static/profill/es6-sham.min.js");
+//     dynamicLoadJs("/static/profill/es7-shim.min.js");
+// }
+
 
