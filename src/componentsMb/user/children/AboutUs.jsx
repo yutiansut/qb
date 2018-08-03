7d35965492f600814f940653f55b5b2478756693
wfy0301 <1398871006@qq.com>
2018-08-03 11:12:39 +0800

    关于我们页面修改

--- src/componentsMb/user/children/AboutUs.jsx
+++ src/componentsMb/user/children/AboutUs.jsx
@@ -5,17 +5,23 @@ export default class AboutUs extends exchangeViewBase {
   constructor(props) {
     super(props);
     this.state = {
-      qbtTrade: []
+      qbtTrade: [],
+      introduceList: [
+        {title:"", url:"", content:""},
+        {title:"", url:"", content:""}
+      ]
     }
     const {controller} = this.props
     controller.setView(this)
     this.getQbtTrade = controller.getQbtTrade.bind(controller)
+    this.addContent = controller.headerController.addContent.bind(controller.headerController)
   }
 
   componentWillMount() {
   }
   componentDidMount() {
     this.getQbtTrade()
+    this.addContent({con: '关于我们'})
   }
 
   render() {
@@ -23,9 +29,19 @@ export default class AboutUs extends exchangeViewBase {
     const language = this.props.controller.configData.language;
     return (
       <div className={`about-us`}>
-        {this.state.qbtTrade && this.state.qbtTrade.map((v,index) =>
-          <img key={index} src={language === "zh-CN" ? v.uc : v.ue}/>
-          )}
+        {/*{this.state.qbtTrade && this.state.qbtTrade.map((v,index) =>*/}
+          {/*<img key={index} src={language === "zh-CN" ? v.uc : v.ue}/>*/}
+          {/*)}*/}
+        <div>
+          <img src="" alt=""/>
+          <p></p>
+          <h1></h1>
+          <ul>
+            <li>
+              <h2></h2>
+            </li>
+          </ul>
+        </div>
         {this.props.controller.configData.activityState ? <a className="invite-btn" onClick={e => {
               history.push(`/mgenrealize/invite/?uid=${controller.userId}`)
           }}>{this.intl.get("activity-rule-42")}</a> : ''}
