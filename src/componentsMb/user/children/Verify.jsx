53ad023365d85984f44a0d278ace72ef0df088e8
Wang Xin <17634029450@163.com>
2018-08-03 09:54:06 +0800

    h5页面两步认证绑定页面样式布局

--- src/componentsMb/user/children/Verify.jsx
+++ src/componentsMb/user/children/Verify.jsx
@@ -1,38 +1,102 @@
-import React from 'react';
-import exchangeViewBase from '../../../components/ExchangeViewBase.jsx';
-import Input from '../../../common/component/Input'
+import React from "react";
+import exchangeViewBase from "../../../components/ExchangeViewBase.jsx";
+import Input from "../../../common/component/Input";
+import Button from "../../../common/component/Button";
 
 export default class AboutUs extends exchangeViewBase {
   constructor(props) {
     super(props);
+    this.state = {};
+    const { controller } = this.props;
+    controller.setView(this);
     this.state = {
-    }
-    const {controller} = this.props
-    controller.setView(this)
-    this.state = {
-      title: ['邮箱两步验证', '谷歌验证码', '短信两步验证' ],
-      type: controller.getQuery('type'),//0 邮箱，1谷歌，2短信
-      account: '',
-      code: '',
-      accountText: ['邮箱地址', '请输入谷歌验证码', '手机地址'],
-      codeText: ['邮箱验证码', '' ,'短信验证码']
-
-    }
+      title: ["邮箱两步验证", "谷歌验证码", "短信两步验证"],
+      // type: controller.getQuery('type'),//0 邮箱，1谷歌，2短信
+      type: 0,
+      account: "",
+      code: "",
+      accountText: ["邮箱地址", "请输入谷歌验证码", "手机号码"],
+      codeText: ["邮箱验证码", "", "短信验证码"],
+      googleCode: ["1", "2", "3", "4", "5", "6"]
+    };
   }
 
-  componentWillMount() {
-  }
-  componentDidMount() {
-
-  }
+  componentWillMount() {}
+  componentDidMount() {}
 
   render() {
-    const {controller, history} = this.props;
-    console.log(this.state.type)
+    const { controller, history } = this.props;
+    let { type, accountText, codeText, googleCode } = this.state;
     return (
-      <div className='user-center-verify'>
-        <Input></Input>
-        <Input></Input>
+      <div className="user-center-verify">
+        {[0, 2].includes(type) ? (
+          <div className="user-center-phonemail">
+            <div className="account">
+              <h3>{accountText[type]}</h3>
+              <Input
+                placeholder="请输入你的手机号码"
+                value={this.state.account}
+                onInput={value => {
+                  this.setState({ account: value });
+                }}
+              />
+            </div>
+            <div className="code">
+              <h3>{codeText[type]}</h3>
+              <Input
+                value={this.state.code}
+                className="code-input"
+                onInput={value => {
+                  this.setState({ code: value });
+                }}
+              />
+              <Button title="获取验证码" type="base" className="send-code" />
+            </div>
+            <Button
+              title="提交"
+              type="base"
+              className="submit"
+              onClick={() => {
+                console.log(999);
+              }}
+            />
+          </div>
+        ) : (
+          <div className="user-center-google">
+            <div className="google">
+              <h3>{accountText[type]}</h3>
+              <div className="clearfix input">
+                {googleCode.map((v, index) => (
+                  <Input
+                    className={`item-code ${index===5 ? 'last-child' : ''}`}
+                    ref={`input${v}`}
+                    key={index}
+                    value={this.state.googleCode[index]}
+                    oriType="password"
+                    onInput={value => {
+                      googleCode[index] = value;
+                      console.log(value, googleCode)
+                      this.setState({ googleCode });
+                    }}
+                  />
+                ))}
+              </div>
+              <Button
+              title="开启Google验证"
+              disable={true}
+              type="base"
+              className="submit"
+              onClick={() => {
+                console.log(999);
+              }}
+            />
+            </div>
+            <p className="tips">
+              温馨提示：<br/>
+              谷歌验证器丢失，请联系客服进行申诉～
+            </p>
+          </div>
+        )}
       </div>
     );
   }
