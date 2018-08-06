f0986830d2addfbd0ad18dd619400856cdee22a4
wfy0301 <1398871006@qq.com>
2018-08-06 17:29:11 +0800

    bug修复

--- src/components/trade/children/TradeDealExchange.jsx
+++ src/components/trade/children/TradeDealExchange.jsx
@@ -28,6 +28,9 @@ export default class TradeDealExchange extends  ExchangeViewBase{
   //   console.log(e.target.value,this.props.prices[this.props.PriceUnit === 'CNY' && 'priceCN' || (this.props.PriceUnit === 'USD' && 'priceEN' || 'price')])
   // }
   render(){
+    // console.log('交易盘1', this.props.PriceUnit)
+    // console.log('交易盘2', this.props.Market)
+    // console.log('交易盘3', this.props.NumUnit)
     let maxValue = this.props.ControllerProps.dealType ? this.props.sellMax : this.props.buyMax;
     return(
         <div className='trade-deal-exchange'>
