75607df7697d8e7f09fa4e8305148a56b34976d6
jiangcj <398017990@qq.com>
2018-08-08 16:51:09 +0800

    深度曲线光滑

--- src/components/trade/depth/js/theme.js
+++ src/components/trade/depth/js/theme.js
@@ -1,15 +1,15 @@
 let Theme={
     _color:{
         background: "#25292b",      // 背景
-        axisLine: "#ccc",             // 坐标轴
-        gridLine: "#555",              //网格线
+        axisLine: "#999",             // 坐标轴
+        gridLine: "#333",              //网格线
         depthLine1: "#2bb789",          //买-深度线
         depthLine1Fill: "rgba(12,159,204,0.3)",         //买-深度线透明填充
-        depthLine2: "#d84747",                          //卖-深度线透明填充
-        depthLine2Fill: "rgba(216,47,47,0.3)",      //卖-柱状线
-        scaleFontColor: "#aaa",                     // 坐标轴文本
-        barFill1: "#2bb789",                        //买-柱状线
-        barFill2: "#d84747",                        //卖-柱状线
+        depthLine2: "#d84747",                          //卖-深度线
+        depthLine2Fill: "rgba(216,47,47,0.3)",      //卖-深度线透明填充
+        scaleFontColor: "#999",                     // 坐标轴文本
+        //barFill1: "#2bb789",                        //买-柱状线
+        //barFill2: "#d84747",                        //卖-柱状线
         infoFontColor: "rgb(43, 183, 137)",       //左上方-鼠标提示文本
     },
     _fonts:"12px Tahoma",                               //字体
