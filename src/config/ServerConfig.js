145f9134681e7ecc4219212e9cd55d9e5efa1ed5
GR <478566374@qq.com>
2018-08-09 19:28:06 +0800

    master

--- src/config/ServerConfig.js
+++ src/config/ServerConfig.js
@@ -3,10 +3,10 @@ export default {
   // host: 'www.qb.com', //正式http主机地址
   // port: 443, //正式主机端口
   hSecure: false,//测试http不开启https
-  host: '192.168.55.105', //测试http主机地址
+  // host: '192.168.55.105', //测试http主机地址
   // host: '192.168.55.111', //书寒http主机地址
   // host: '192.168.55.104', //架构http主机地址
-  // host: '192.168.55.108', //肖宇http主机地址
+  host: '192.168.55.108', //肖宇http主机地址
   port: 80, //测试,书寒主机端口
   // port: 90, //架构主机端口
 
@@ -16,9 +16,9 @@ export default {
   // wsport: 443, //正式websocket主机端口
   wSecure: false,//测试websocket不开启wss
   // wshost:'192.168.55.104', //架构websocket主机地址
-  // wshost:'192.168.55.108', //肖宇websocket主机地址
+  wshost:'192.168.55.108', //肖宇websocket主机地址
   // wshost:'192.168.55.111', ///书寒websocket主机地址
-  wshost:'192.168.55.105', //测试websocket主机地址
+  // wshost:'192.168.55.105', //测试websocket主机地址
   wsport: 8090, //测试,书寒websocket主机端口
   // wsport: 9090, //架构websocket主机端口
 }
