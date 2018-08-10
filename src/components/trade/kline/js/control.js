cc58e6103d97bad9dd18eb9e3bf19de8cbd72c49
jiangcj <398017990@qq.com>
2018-08-10 14:46:59 +0800

    修改深度累计量误差

--- src/components/trade/kline/js/control.js
+++ src/components/trade/kline/js/control.js
@@ -3,7 +3,8 @@ import {ChartManager} from './chart_manager'
 import {ChartSettings} from './chart_settings'
 import {DefaultTemplate, Template} from './templates'
 import {MEvent} from './mevent'
-import $ from "../lib/jquery.min"
+import $ from "../lib/dom"
+//import $ from "../lib/jquery.min"
 
 export class Control {
 
@@ -117,9 +118,10 @@ export class Control {
     }
 
     static onSize(w, h) {
+
         let width = w || window.innerWidth;
-        let chartWidth=width;
         let height = h || window.innerHeight;
+        let chartWidth=width;
         let container = $(Kline.instance.element);
         container.css({
             width: width + 'px',
@@ -129,8 +131,8 @@ export class Control {
         let toolPanel = $('#chart_toolpanel');
         let canvasGroup = $('#chart_canvasGroup');
         let tabBar = $('#chart_tabbar');
-        let toolPanelShown = toolPanel[0].style.display !== 'inline' ? false : true;
-        let tabBarShown = tabBar[0].style.display !== 'block' ? false : true;
+        let toolPanelShown = toolPanel[0].style.display === 'inline';
+        let tabBarShown = tabBar[0].style.display === 'block';
         let toolBarRect = {};
         toolBarRect.x = 0;
         toolBarRect.y = 0;
@@ -169,34 +171,39 @@ export class Control {
         canvasGroup.css({
             left: canvasGroupRect.x + 'px',
             top: canvasGroupRect.y + 'px',
-            // width: canvasGroupRect.w + 'px',
+            width: canvasGroupRect.w + 'px',
             height: canvasGroupRect.h + 'px'
         });
+
         let mainCanvas = $('#chart_mainCanvas')[0];
         let overlayCanvas = $('#chart_overlayCanvas')[0];
 
-        /*
+
         let dpr = window.devicePixelRatio;
         console.log("devicePixelRatio:\n",dpr);
-
-        mainCanvas.width = canvasGroupRect.w * dpr;
-        mainCanvas.height = canvasGroupRect.h * dpr;
-        overlayCanvas.width = canvasGroupRect.w * dpr;
-        overlayCanvas.height = canvasGroupRect.h * dpr;
-        $(mainCanvas).css({
-            width: canvasGroupRect.w,
-            height: canvasGroupRect.h,
-        });
-        $(overlayCanvas).css({
-            width: canvasGroupRect.w,
-            height: canvasGroupRect.h,
-        });
-        */
+        //dpr = 0.5;
 
         mainCanvas.width = canvasGroupRect.w;
         mainCanvas.height = canvasGroupRect.h;
         overlayCanvas.width = canvasGroupRect.w;
         overlayCanvas.height = canvasGroupRect.h;
+        $(mainCanvas).css({
+            width: canvasGroupRect.w + "px",
+            height: canvasGroupRect.h + "px",
+        });
+        $(overlayCanvas).css({
+            width: canvasGroupRect.w + "px",
+            height: canvasGroupRect.h + "px",
+        });
+
+        console.log("k线-尺寸：",canvasGroupRect, dpr, mainCanvas.width, mainCanvas.height, $(mainCanvas));
+
+        /*
+        mainCanvas.width = canvasGroupRect.w;
+        mainCanvas.height = canvasGroupRect.h;
+        overlayCanvas.width = canvasGroupRect.w;
+        overlayCanvas.height = canvasGroupRect.h;
+        */
 
         if (tabBarShown) {
             tabBar.css({
@@ -208,13 +215,13 @@ export class Control {
         }
         let dlgSettings = $("#chart_parameter_settings");
         dlgSettings.css({
-            left: (window.innerWidth - dlgSettings.width()) >> 1,
-            top: (window.innerHeight - dlgSettings.height()) >> 1
+            left: (window.innerWidth - dlgSettings.width()) >> 1 + "px",
+            top: (window.innerHeight - dlgSettings.height()) >> 1 + "px"
         });
         let dlgLoading = $("#chart_loading");
         dlgLoading.css({
-            left: (chartWidth - dlgLoading.width()) >> 1,
-            top: (height - dlgLoading.height()) >> 2
+            left: (chartWidth - dlgLoading.width()) >> 1 + "px",
+            top: (height - dlgLoading.height()) >> 2 + "px"
         });
         let domElemCache = $('#chart_dom_elem_cache');
         let rowTheme = $('#chart_select_theme')[0];
@@ -304,11 +311,7 @@ export class Control {
             tmp.theme = 'Light';
             ChartSettings.save();
         }
-        let a = {};
-        a.command = "set current themes";
-        a.content = name;
-        $('#chart_output_interface_text').val(JSON.stringify(a));
-        $('#chart_output_interface_submit').submit();
+
         new MEvent().raise(name);
         ChartManager.instance.redraw();
 
--- src/components/trade/kline/lib/dom.js
+++ src/components/trade/kline/lib/dom.js
@@ -2,132 +2,377 @@
  * Dom 模拟jquery常用的操作
  * 用于替换jquery
  */
-class Dom{
 
+class Dom{
     constructor(sel){
         if(typeof sel === "object"){
-            this.el = sel;
-        }else{
-            this.el = document.querySelector(sel);
-        }
-    }
-
-    bind(event,handler){
-        let el = this.el;
-        el.addEventListener(event,handler.bind(el));
-        return this;
-    }
-
-    attr(name,value){
-        let el = this.el;
-        el.setAttribute(name,value);
-        return this;
-    }
-
-    css(name,value){
-        let el = this.el;
-        let name1=name.replace(/-./g , function(str){
-            return str.substr(1).toUpperCase();
-        });
-        el.style[name1]=value;
-        return this;
-    }
-
-    cssA(obj){
-        Object.keys(obj).forEach(key=>{
-            this.css(key , obj[key]);
-        });
-        return this;
-    }
-
-   removeClass(cls){
-        let el = this.el;
-        el.className = el.className.replace(new RegExp(cls,"g"),"");
-        el.className = el.className.replace(/^\s+|\s+$/g, '');
-        return this;
-   }
-
-   addClass(cls){
-        let el = this.el;
-        el.className += " " + cls;
-        el.className = el.className.replace(/^\s+|\s+$/g, '');
-        return this;
-   }
-
-   parent(){
-        let el = this.el;
-        this.el = el.parentNode;
-        return this;
-   }
-
-   html(txt){
-        let el = this.el;
-        el.innerHTML = txt;
-        return this;
-   }
-
-   text(txt){
-        let el = this.el;
-        el.innerText = txt;
-        return this;
-   }
-
-   before(nEl){
-       let el = this.el;
-       let parent = el.parentNode;
-       for(let i=0;i<parent.childNodes.length;i++){
-           if(parent.childNodes[i]===el){
-               parent.insertBefore(nEl,parent.childNodes[i]);
-           }
-       }
-       return this;
-   }
-
-   after(nEl){
-        let el = this.el;
-        let parent = el.parentNode;
-        for(let i=0;i<parent.childNodes.length;i++){
-            if(parent.childNodes[i]===el){
-                if(i===parent.childNodes.length-1){
-                    parent.appendChild(nEl);
-                }else{
-                    parent.insertBefore(nEl,parent.childNodes[i+1]);
-                }
+            if(sel instanceof Dom){
+                return sel;
+            }else if(sel instanceof Array){
+                this.el = sel;
+            }else{
+                this.el = [sel];
             }
+        }else{
+            this.el = document.querySelectorAll(sel);
+        }
+        //
+        this.registerEvent("mouseover");
+        this.registerEvent("mouseout");
+        this.registerEvent("mousemove");
+        this.registerEvent("mouseleave");
+        this.registerEvent("mouseup");
+        this.registerEvent("mousedown");
+        this.registerEvent("keyup");
+        this.registerEvent("keydown");
+        this.registerEvent("click");
+        this.registerEvent("change");
+        //
+        for(let i=0;i<this.el.length;i++){
+            this[i] = this.el[i];
+        }
+        //事件池
+        if(!window.DomEventHandlers){
+            window.DomEventHandlers = {};
+        }
+    }
+
+    attr(){
+        if(arguments.length === 1){
+            if(typeof arguments[0] === "object"){
+                this.el.forEach(el=>{
+                    Object.keys(arguments[0]).forEach(key=>{
+                        el.setAttribute(key, arguments[0][key]);
+                    });
+                });
+            }
+            if(typeof arguments[0] === "string"){
+                return this.el[0].getAttribute(arguments[0]);
+            }
+        }else if(arguments.length === 2){
+            this.el.forEach(el=>{
+                el.setAttribute(arguments[0],arguments[1]);
+            })
         }
         return this;
-   }
+    }
 
-   append(nEl){
-        let el=this.el;
-        el.appendChild(nEl);
+    css() {
+        if (arguments.length === 1) {
+            if (typeof arguments[0] === "object") {
+                this.el.forEach(el => {
+                    Object.keys(arguments[0]).forEach(key => {
+                        let nKey = key.replace(/-(\w)/g, (all, letter) => letter.toUpperCase());
+                        el.style[nKey] = arguments[0][key];
+                    });
+                });
+            }
+            if (typeof arguments[0] === "string") {
+                let nKey = arguments[0].replace(/-(\w)/g, (all, letter) => letter.toUpperCase());
+                return this.el[0].style[nKey];
+            }
+        } else if (arguments.length === 2) {
+            this.el.forEach(el => {
+                let nKey = arguments[0].replace(/-(\w)/g, (all, letter) => letter.toUpperCase());
+                el.style[nKey] = arguments[1];
+            })
+        }
         return this;
-   }
-
-   //******************************************************************************************
-
-    find(sel){
-        let el = this.el;
-        return el.querySelector(sel);
     }
 
-    findA(sel){
-        let el = this.el;
-        return el.querySelectorAll(sel);
+    removeClass(name){
+        this.el.forEach(el=>{
+            el.classList.remove(name);
+        });
+        return this;
     }
 
-    getEl(){
-        return this.el;
+    addClass(name){
+        this.el.forEach(el=>{
+            el.classList.add(name);
+        });
+        return this;
+    }
+
+    hasClass(name){
+        return this.el[0].classList.contains(name);
+    }
+
+    html(){
+        if(arguments.length===0){
+            return this.el[0].innerHTML;
+        }else if(arguments.length===1){
+            this.el.forEach(el=>{
+                el.innerHTML = arguments[0];
+            })
+        }
+        return this;
+    }
+
+    text(){
+        if(arguments.length===0){
+            return this.el[0].innerText;
+        }else if(arguments.length===1){
+            this.el.forEach(el=>{
+                el.innerText = arguments[0];
+            })
+        }
+        return this;
     }
 
     width(){
-        let el = this.el;
-        return el.offsetWidth;
+        if(arguments.length===0){
+            return this.el[0].offsetWidth;
+        }else if(arguments.length===1){
+            this.el.forEach(el=>{
+                el.style.width = arguments[0] + "px";
+            })
+        }
+        return this;
     }
 
     height(){
-        let el = this.el;
-        return el.offsetHeight;
+        if(arguments.length===0){
+            return this.el[0].offsetHeight;
+        }else if(arguments.length===1){
+            this.el.forEach(el=>{
+                el.style.height = arguments[0] + "px";
+            })
+        }
+        return this;
+    }
+
+    offset(){
+        if(arguments.length===0){
+            let el = this.el[0];
+            let left = 0,top = 0;
+            while(el !== document.body){
+                left += el.offsetLeft;
+                top += el.offsetTop;
+                el = el.parentNode;
+            }
+            return {left: left, top: top};
+        }else{
+            console.log("Dom.js：offset不支持此用法");
+        }
+        return this;
+    }
+
+    val(){
+        if(arguments.length===0){
+            return this.el[0].value;
+        }else if(arguments.length===1){
+            this.el.forEach(el=>{
+                el.value = arguments[0];
+            })
+        }
+        return this;
+    }
+
+    before(html){
+        if(typeof html === "string"){
+            this.el.forEach(el=>{
+                el.insertAdjacentHTML("beforeBegin",html);
+            });
+        }else{
+            this.el.forEach(el=>{
+                el.insertAdjacentElement("beforeBegin",html);
+            });
+        }
+        return this;
+    }
+
+    after(html){
+        if(typeof html === "string"){
+            this.el.forEach(el=>{
+                el.insertAdjacentHTML("afterEnd",html);
+            });
+        }else{
+            this.el.forEach(el=>{
+                el.insertAdjacentElement("afterEnd",html);
+            });
+        }
+        return this;
+    }
+
+    append(html){
+        if(typeof html === "string"){
+            this.el.forEach(el=>{
+                el.insertAdjacentHTML("beforeEnd",html);
+            });
+        }else{
+            this.el.forEach(el=>{
+                el.insertAdjacentElement("beforeEnd",html);
+            });
+        }
+        return this;
+    }
+
+    prepend(html){
+        if(typeof html === "string"){
+            this.el.forEach(el=>{
+                el.insertAdjacentHTML("afterBegin",html);
+            });
+        }else{
+            this.el.forEach(el=>{
+                el.insertAdjacentElement("afterBegin",html);
+            });
+        }
+        return this;
+    }
+
+    //-------------------------------------------------------
+    parent(){
+        return new Dom(this.el[0].parentNode);
+    }
+
+    find(sel){
+        let newEl=[];
+        this.el.forEach(el=>{
+            el.querySelectorAll(sel).forEach(el2=>{
+                newEl.push(el2);
+            });
+        });
+        return new Dom(newEl);
+    }
+
+    next(){
+        let newEl = [];
+        this.el.forEach(el=>{
+            newEl.push(el.nextSibling);
+        });
+        return new Dom(newEl)
+    }
+
+    prev(){
+        let newEl = [];
+        this.el.forEach(el=>{
+            newEl.push(el.previousSibling);
+        });
+        return new Dom(newEl);
+    }
+
+    children(){
+        if(arguments.length===0){
+            let newEl = [];
+            this.el.forEach(el=>{
+                el.childNodes.forEach(el2=>{
+                    newEl.push(el2);
+                });
+            });
+            return new Dom(newEl);
+        }else if(arguments.length===1){
+            let newEl=[];
+            this.el.forEach(el=>{
+                el.querySelectorAll(arguments[0]).forEach(el2=>{
+                    el2.parentNode === el && newEl.push(el2);
+                });
+            });
+            return new Dom(newEl);
+        }
+    }
+
+    //------------------------------------------------------
+
+    each(func){
+        this.el.forEach((item,index)=>{
+            func.bind(item)(index,item);
+        })
+    }
+
+    //--------------------------------------------------------class
+    //注册事件
+    registerEvent(name){
+        this[name]= (func)=>{
+            this.bind(name,func);
+            return this;
+        }
+    }
+
+    on(name,func){
+        return this.bind(name,func);
+    }
+
+    bind(name,func){
+        if(name === "mousewheel"){
+            this.bindMousewheel(func);
+        }else{
+            this.bindEvent(name,func);
+        }
+        return this;
+    }
+
+    isFirefox(){
+        if(navigator.userAgent.indexOf('Firefox') >= 0){
+            return true;
+        }
+        return false;
+    }
+
+    bindEvent(name,func){
+        this.el.forEach(el=>{
+            let eid = el["eid"];
+            if(!eid){
+                eid = Date.now();
+                el["eid"] = eid;
+            }
+            let handler = (event)=>{
+                func.bind(el)(event);
+            };
+            el.addEventListener(name, handler);
+            !window.DomEventHandlers[eid] && (window.DomEventHandlers[eid] = []);
+            window.DomEventHandlers[eid].push(handler);
+            //console.log("k线-绑定事件：",name,el,eid);
+            //console.log("k线-事件池：",window.DomEventHandlers);
+        });
+    }
+
+    bindMousewheel(func){
+        this.el.forEach(el=>{
+            let eid = el["eid"];
+            if(!eid){
+                eid = Date.now();
+                el["eid"] = eid;
+            }
+            if(this.isFirefox()){       //火狐
+                let handler = (event)=>{
+                    let delta = event.detail>0 ? -1 : 1;
+                    func.bind(el)(event,delta);
+                    event.preventDefault();
+                    event.stopPropagation();
+                };
+                el.addEventListener("DOMMouseScroll",handler);
+                !window.DomEventHandlers[eid] && (window.DomEventHandlers[eid] = []);
+                window.DomEventHandlers[eid].push(handler);
+
+            }else{                      //其他
+                let handler = (event)=>{
+                    let delta = event.wheelDelta>0 ? 1 : -1;
+                    func.bind(el)(event, delta);
+                    event.preventDefault();
+                    event.stopPropagation();
+                };
+                el.addEventListener("mousewheel",handler);
+                !window.DomEventHandlers[eid] && (window.DomEventHandlers[eid] = []);
+                window.DomEventHandlers[eid].push(handler);
+            }
+        });
+    }
+
+    unbind(){
+        if(arguments.length===1){
+            this.el.forEach(el=>{
+                let eid = el["eid"];
+                let handlers = window.DomEventHandlers[eid];
+                handlers.forEach(hdl =>{
+                    el.removeEventListener(arguments[0],hdl);
+                });
+            });
+        }else if(arguments.length===2){
+            this.el.forEach(el=>{
+                el.removeEventListener(arguments[0],arguments[1]);
+            });
+        }
     }
 
 }
