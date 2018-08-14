d68fb308a49dd6f919584db58b1fbb858fc8a05d
DongJin <dongjin@mixotc.com>
2018-08-14 09:25:19 +0800

    修复k线

--- src/config/ServerConfig.js
+++ src/config/ServerConfig.js
@@ -1,24 +1,24 @@
 export default {
-  hSecure: true,//正式http开启https
-  host: 'www.qb.com', //正式http主机地址
-  port: 443, //正式主机端口
-  // hSecure: false,//测试http不开启https
-  // host: '192.168.55.105', //测试http主机地址
+  // hSecure: true,//正式http开启https
+  // host: 'www.qb.com', //正式http主机地址
+  // port: 443, //正式主机端口
+  hSecure: false,//测试http不开启https
+  host: '192.168.55.105', //测试http主机地址
   // host: '192.168.55.111', //书寒http主机地址
   // host: '192.168.55.104', //架构http主机地址
   // host: '192.168.55.108', //肖宇http主机地址
-  //port: 80, //测试,书寒主机端口
+  port: 80, //测试,书寒主机端口
   // port: 90, //架构主机端口
 
 
-  wSecure: true,//正式websocket开启wss
-  wshost:'www.qb.com', //正式websocket主机地址
-  wsport: 443, //正式websocket主机端口
-  // wSecure: false,//测试websocket不开启wss
+  // wSecure: true,//正式websocket开启wss
+  // wshost:'www.qb.com', //正式websocket主机地址
+  // wsport: 443, //正式websocket主机端口
+  wSecure: false,//测试websocket不开启wss
   // wshost:'192.168.55.104', //架构websocket主机地址
   // wshost:'192.168.55.108', //肖宇websocket主机地址
   // wshost:'192.168.55.111', ///书寒websocket主机地址
-  // wshost:'192.168.55.105', //测试websocket主机地址
-  // wsport: 8090, //测试,书寒websocket主机端口
+  wshost:'192.168.55.105', //测试websocket主机地址
+  wsport: 8090, //测试,书寒websocket主机端口
   // wsport: 9090, //架构websocket主机端口
 }
